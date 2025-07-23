dd6f5586a439efea7fbf190ca3425f1799548043
23f3003687 <23f3003687@ds.study.iitm.ac.in>
2025-07-23 22:53:57 +0530

    slight color change

--- frontend/src/components/register.vue
+++ frontend/src/components/register.vue
@@ -7,7 +7,7 @@
       class="card shadow-sm p-4 w-100"
       style="
         max-width: 400px;
-        background-color: rgba(255, 255, 255, 0.8);
+        background-color: rgba(178, 222, 240, 0.68);
         border: none;
       "
     >

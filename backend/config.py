f842bba452a88a2ad7340b6ee602ee2294d453f6
23f3003687 <23f3003687@ds.study.iitm.ac.in>
2025-12-25 18:57:44 +0530

    default values from environment variables

--- backend/config.py
+++ backend/config.py
@@ -4,16 +4,16 @@ load_dotenv()
 
 class Config:
     # Celery & Redis
-    BROKER_URL = os.environ.get("BROKER_URL", "redis://localhost:6379/0")
-    RESULT_BACKEND = os.environ.get("RESULT_BACKEND", "redis://localhost:6379/0")
+    BROKER_URL = os.environ.get("  ")
+    RESULT_BACKEND = os.environ.get(" ")
     # SQLite DB
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI", "sqlite:///quiz_master.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(" ")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
      # Email SMTP Settings
-    MAIL_SERVER = os.environ.get("MAIL_SERVER", "smtp.gmail.com")
-    MAIL_PORT = int(os.environ.get("MAIL_PORT", 587))
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME", "quizmasterv2@gmail.com")  
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")     
+    MAIL_SERVER = os.environ.get(" ")
+    MAIL_PORT = int(os.environ.get( ))
+    MAIL_USERNAME = os.environ.get(" ")  
+    MAIL_PASSWORD = os.environ.get(" ")     
     # Secret Keys
-    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "supersecretkey")
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+    SECRET_KEY = os.environ.get(" ")
+    JWT_SECRET_KEY = os.environ.get(" ")
